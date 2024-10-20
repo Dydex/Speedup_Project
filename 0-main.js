313feb0e892ac48d4d9c7cd05595940b661266df
AjalaDp <supremeajala@gmail>
2024-10-20 19:48:16 +0100

    first commit

---
+++ 0-main.js
@@ -0,0 +1,20 @@
+// const name = "Dp"
+// name = "dumdevs"
+// console.log("my name is " + name)
+
+// const myObject = {
+//     type: "object",
+//     value: 12
+// };
+
+// myObject.value = 89;
+
+// console.log(myObject)
+// const guy = { name: 'dolapo', gender: 'male' }
+// console.log(guy)
+
+function pLus(a, bOy) {
+    return a * bOy;
+}
+
+console.log(pLus(3, 5))
---
+++ 0-print.py
@@ -0,0 +1,12 @@
+
+
+# number = 4
+# print(type(number))
+"""
+I love python
+"""
+# This clean
+
+name = input("what is ur name ? ")
+welcome = "Nice to meet u " + (name) + "!"
+print("{}".format(welcome))
