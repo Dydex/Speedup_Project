8280d88600c7d68901a8c8c64d6e9f2f46557818
AjalaDp <supremeajala@gmail>
2024-11-01 09:42:27 +0100

    javascript

--- 0-main.js
+++ 0-main.js
@@ -1,20 +1,9 @@
-// const name = "Dp"
-// name = "dumdevs"
-// console.log("my name is " + name)
-
-// const myObject = {
-//     type: "object",
-//     value: 12
-// };
-
-// myObject.value = 89;
-
-// console.log(myObject)
-// const guy = { name: 'dolapo', gender: 'male' }
-// console.log(guy)
-
-function pLus(a, bOy) {
-    return a * bOy;
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
 }
 
-console.log(pLus(3, 5))
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
